7b4b3423dbbae86e80c349c5076f7ddb5fb939df
Tom Nielsen <tanielsen@gmail.com>
2022-02-22 22:14:17 +0000

    first wip

---
+++ index.js
@@ -0,0 +1,250 @@
+const Field = require("@saltcorn/data/models/field");
+const Table = require("@saltcorn/data/models/table");
+const Form = require("@saltcorn/data/models/form");
+const View = require("@saltcorn/data/models/view");
+const Workflow = require("@saltcorn/data/models/workflow");
+
+const {
+  text,
+  div,
+  h5,
+  style,
+  a,
+  script,
+  pre,
+  domReady,
+  i,
+  text_attr,
+} = require("@saltcorn/markup/tags");
+const { readState } = require("@saltcorn/data//plugin-helper");
+const configuration_workflow = () =>
+  new Workflow({
+    steps: [
+      {
+        name: "views",
+        form: async (context) => {
+          const table = await Table.findOne({ id: context.table_id });
+          const fields = await table.getFields();
+
+          return new Form({
+            fields: [
+              {
+                name: "title_field",
+                label: "Title field",
+                type: "String",
+                sublabel: "Event label displayed on the task.",
+                required: true,
+                attributes: {
+                  options: fields
+                    .filter((f) => f.type.name === "String")
+                    .map((f) => f.name),
+                },
+              },
+              {
+                name: "row_field",
+                label: "Row field",
+                type: "String",
+                sublabel: "Field indicating the row",
+                required: true,
+                attributes: {
+                  options: fields.map((f) => f.name),
+                },
+              },
+              {
+                name: "start_field",
+                label: "Start time field",
+                type: "String",
+                sublabel:
+                  "The table needs a fields of type 'Date' to track start times.",
+                required: true,
+                attributes: {
+                  options: fields
+                    .filter((f) => f.type.name === "Date")
+                    .map((f) => f.name),
+                },
+              },
+              {
+                name: "end_field",
+                label: "End time field",
+                type: "String",
+                sublabel:
+                  "Specify an end field of type 'Date' or a duration field",
+                required: false,
+                attributes: {
+                  options: fields
+                    .filter((f) => f.type.name === "Date")
+                    .map((f) => f.name),
+                },
+              },
+
+              {
+                name: "duration_field",
+                label: "Duration field",
+                type: "String",
+                sublabel:
+                  "A fields of type 'Int' or 'Float' to denote the duration of the event.",
+                required: false,
+                attributes: {
+                  options: fields
+                    .filter(
+                      (f) => f.type.name === "Int" || f.type.name === "Float"
+                    )
+                    .map((f) => f.name),
+                },
+              },
+              {
+                name: "duration_units",
+                label: "Duration units",
+                type: "String",
+                sublabel: "Units of duration field, if specified",
+                required: true,
+                attributes: {
+                  options: "Seconds,Minutes,Hours,Days",
+                },
+              },
+            ],
+          });
+        },
+      },
+    ],
+  });
+
+const get_state_fields = async (table_id, viewname, { show_view }) => {
+  const table_fields = await Field.find({ table_id });
+  return table_fields
+    .filter((f) => !f.primary_key)
+    .map((f) => {
+      const sf = new Field(f);
+      sf.required = false;
+      return sf;
+    });
+};
+
+const run = async (
+  table_id,
+  viewname,
+  {
+    start_field,
+    duration_field,
+    duration_units,
+    title_field,
+    row_field,
+    end_field,
+  },
+  state,
+  extraArgs
+) => {
+  const table = await Table.findOne({ id: table_id });
+  const fields = await table.getFields();
+  const row_fld = fields.find((f) => f.name === row_field);
+  readState(state, fields);
+  const role = extraArgs.req.isAuthenticated()
+    ? extraArgs.req.user.role_id
+    : 10;
+  const qstate = await stateFieldsToWhere({ fields, state });
+  const dbrows = await table.getRows(qstate);
+  const chart_rows = {};
+  const tasks = dbrows.map((r) => {
+    if (!chart_rows[r[row_field]]) {
+      chart_rows[r[row_field]] = {
+        id: r[row_field],
+        label: row_fld.is_fkey ? "" : r[row_field],
+      };
+    }
+    return {
+      id: r.id,
+      resourceId: r[row_field],
+      label: r[title_field],
+      from: r[start_field],
+      to: end_field ? r[end_field] : r[start_field],
+    };
+  });
+  return (
+    div({ id: "example-gantt" }) +
+    script(
+      domReady(`const gantt = new SvelteGantt({ 
+    target: document.getElementById('example-gantt'), 
+    props: {
+      tasks:${JSON.stringify(tasks)},
+      rows:${JSON.stringify(Object.values(chart_rows))}
+    }});`)
+    )
+  );
+};
+
+//card has been dragged btw columns
+const set_card_value = async (
+  table_id,
+  viewname,
+  { column_field, position_field },
+  body,
+  { req }
+) => {
+  const table = await Table.findOne({ id: table_id });
+  const role = req.isAuthenticated() ? req.user.role_id : 10;
+  if (role > table.min_role_write) {
+    return { json: { error: "not authorized" } };
+  }
+  let colval = body[column_field];
+  const fields = await table.getFields();
+  const column_field_field = fields.find((f) => f.name === column_field);
+  if (column_field_field && column_field_field.type === "Key") {
+    const reftable = await Table.findOne({
+      name: column_field_field.reftable_name,
+    });
+    const refrow = await reftable.getRow({
+      [column_field_field.attributes.summary_field]: body[column_field],
+    });
+    colval = refrow.id;
+  }
+  if (position_field) {
+    var newpos;
+    const exrows = await table.getRows(
+      { [column_field]: colval },
+      { orderBy: position_field }
+    );
+    const before_id = parseInt(body.before_id);
+    if (before_id) {
+      const before_ix = exrows.findIndex((row) => row.id === before_id);
+      if (before_ix === 0) newpos = exrows[0][position_field] - 1;
+      else
+        newpos =
+          (exrows[before_ix - 1][position_field] +
+            exrows[before_ix][position_field]) /
+          2;
+    } else {
+      if (exrows.length > 0)
+        newpos = exrows[exrows.length - 1][position_field] + 1;
+      else newpos = Math.random();
+    }
+
+    await table.updateRow(
+      { [column_field]: colval, [position_field]: newpos },
+      parseInt(body.id)
+    );
+  } else {
+    await table.updateRow({ [column_field]: colval }, parseInt(body.id));
+  }
+
+  return { json: { success: "ok" } };
+};
+
+module.exports = {
+  headers: [
+    {
+      script: "/plugins/public/svelte-gantt/index.iife.min.js",
+    },
+  ],
+  sc_plugin_api_version: 1,
+  plugin_name: "svelte-gantt",
+  viewtemplates: [
+    {
+      name: "Gantt chart",
+      display_state_form: false,
+      get_state_fields,
+      configuration_workflow,
+      run,
+      routes: { set_card_value },
+    },
+  ],
+};
