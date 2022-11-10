ed1958773b871842457cc82f5d13a42d17204908
Tom Nielsen <tanielsen@gmail.com>
2022-11-10 14:56:41 +0000

    wip resource

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const {
 const { features } = require("@saltcorn/data/db/state");
 
 const moment = require("moment"); // require
+const { calcSpanProps } = require("./common");
 
 //https://stackoverflow.com/a/13532993 - not used
 function shadeColor(color, percent) {
@@ -833,144 +834,8 @@ const run = async (
   var spanDays = moment(last_end).diff(first_start, "days");
   var spanMonths = moment(last_end).diff(first_start, "months");
   //console.log({ spanDays, spanMonths });
-  const spanProps =
-    spanMonths > 100
-      ? {
-        columnOffset: 14,
-        columnUnit: "day",
-        magnetUnit: "day",
-        magnetOffset: 1,
-        headers: [
-          {
-            unit: "year",
-            format: "YYYY",
-            offset: Math.floor(spanMonths / 120),
-          },
-        ],
-      }
-      : spanMonths > 36
-        ? {
-          columnOffset: 14,
-          columnUnit: "day",
-          magnetUnit: "day",
-          magnetOffset: 1,
-          alignStartTo: "year",
-          headers: [
-            {
-              unit: "year",
-              format: "YYYY",
-              offset: 1,
-            },
-            { unit: "month", format: "[Q]Q", offset: 3 },
-          ],
-        }
-        : spanMonths > 12
-          ? {
-            columnOffset: 14,
-            columnUnit: "day",
-            magnetUnit: "day",
-            magnetOffset: 1,
-            alignStartTo: "quarter",
-            headers: [
-              {
-                unit: "month",
-                format: "YYYY[Q]Q",
-                offset: 3,
-              },
-              { unit: "month", format: "MM", offset: 1 },
-            ],
-          }
-          : spanDays > 80
-            ? {
-              columnOffset: 14,
-              columnUnit: "day",
-              magnetUnit: "day",
-              magnetOffset: 1,
-              alignStartTo: "month",
-              headers: [
-                {
-                  unit: "month",
-                  format: "MMM YYYY",
-                  offset: 1,
-                },
-                { unit: "week", format: "[W]w", offset: spanDays > 200 ? 2 : 1 },
-              ],
-            }
-            : spanDays > 26
-              ? {
-                columnOffset: 14,
-                columnUnit: "day",
-                magnetUnit: "day",
-                magnetOffset: 1,
-                alignStartTo: "week",
-                headers: [
-                  { unit: "week", format: "MM/YYYY [W]w", offset: 1 },
-                  { unit: "day", format: "DD", offset: Math.ceil(spanDays / 50) },
-                ],
-              }
-              : spanDays > 20
-                ? {
-                  columnOffset: 1,
-                  columnUnit: "day",
-                  magnetUnit: "day",
-                  magnetOffset: 1,
-                  alignStartTo: "week",
-                  headers: [
-                    { unit: "week", format: "MMM YYYY [Week ]w", offset: 1 },
+  const spanProps = calcSpanProps(spanMonths, spanDays)
 
-                    { unit: "day", format: "ddDD", offset: 1 },
-                  ],
-                }
-                : spanDays > 8
-                  ? {
-                    columnOffset: 1,
-                    columnUnit: "day",
-                    magnetUnit: "day",
-                    magnetOffset: 1,
-                    alignStartTo: "week",
-                    headers: [
-                      { unit: "week", format: "MMM YYYY [Week ]w", offset: 1 },
-
-                      { unit: "day", format: "ddd DD", offset: 1 },
-                    ],
-                  }
-                  : spanDays > 4
-                    ? {
-                      columnOffset: 3,
-                      columnUnit: "hour",
-                      magnetUnit: "hour",
-                      magnetOffset: 1,
-                      alignStartTo: "day",
-
-                      headers: [
-                        { unit: "day", format: "ddd DD MMM YYYY", offset: 1 },
-                        { unit: "hour", format: "H:mm", offset: Math.ceil(spanDays) },
-                      ],
-                    }
-                    : spanDays > 1
-                      ? {
-                        columnOffset: 60,
-                        columnUnit: "minute",
-                        magnetUnit: "minute",
-                        magnetOffset: 30,
-                        alignStartTo: "day",
-
-                        headers: [
-                          { unit: "day", format: "dddd DD MMM YYYY", offset: 1 },
-                          { unit: "hour", format: "H:mm", offset: Math.ceil(spanDays) },
-                        ],
-                      }
-                      : {
-                        columnOffset: 30,
-                        columnUnit: "minute",
-                        magnetUnit: "minute",
-                        magnetOffset: 15,
-                        alignStartTo: "day",
-                        headers: [
-                          { unit: "day", format: "dddd DD MMM YYYY", offset: 1 },
-                          { unit: "hour", format: "H:mm", offset: Math.ceil(spanDays) },
-                        ],
-                      };
   let dependencies = [];
   if (dependency_table && dependency_from_field && dependency_to_field) {
     const myIds = tasks.map((t) => t.id);
@@ -1266,6 +1131,7 @@ module.exports = {
       run,
       routes: { change_task, add_dependency },
     },
+    require("./resources")
   ],
 };
 
---
+++ resources.js
@@ -0,0 +1,277 @@
+const Field = require("@saltcorn/data/models/field");
+const Table = require("@saltcorn/data/models/table");
+const Form = require("@saltcorn/data/models/form");
+const View = require("@saltcorn/data/models/view");
+const Workflow = require("@saltcorn/data/models/workflow");
+const {
+  eval_expression,
+  freeVariables,
+} = require("@saltcorn/data/models/expression");
+
+const {
+  text,
+  div,
+  h5,
+  style,
+  a,
+  script,
+  pre,
+  domReady,
+  i,
+  text_attr,
+  button,
+  input,
+  label,
+} = require("@saltcorn/markup/tags");
+const {
+  readState,
+  stateFieldsToWhere,
+  add_free_variables_to_joinfields,
+} = require("@saltcorn/data//plugin-helper");
+const { features } = require("@saltcorn/data/db/state");
+
+const moment = require("moment"); // require
+const { calcSpanProps } = require("./common");
+
+const configuration_workflow = () =>
+  new Workflow({
+    steps: [
+      {
+        name: "Views and fields",
+        form: async (context) => {
+          const table = await Table.findOne({ id: context.table_id });
+          const fields = await table.getFields();
+          const colour_options = fields
+            .filter((f) => f.type.name === "Color")
+            .map((f) => f.name);
+          for (const field of fields) {
+            if (field.is_fkey) {
+              const reftable = Table.findOne({
+                name: field.reftable_name,
+              });
+              const reffields = await reftable.getFields();
+              reffields
+                .filter((f) => f.type.name === "Color")
+                .forEach((f) => colour_options.push(`${field.name}.${f.name}`));
+            }
+          }
+          const edit_views = await View.find_table_views_where(
+            context.table_id,
+            ({ state_fields, viewtemplate, viewrow }) =>
+              viewrow.name !== context.viewname
+          );
+          const edit_view_opts = edit_views.map((v) => v.name);
+          const { child_field_list, child_relations } =
+            await table.get_child_relations();
+          const dependency_table_opts = new Set();
+          child_relations.forEach(({ table }) =>
+            dependency_table_opts.add(table.name)
+          );
+          return new Form({
+            fields: [
+              {
+                name: "row_field",
+                label: "Row field",
+                type: "String",
+                sublabel: "Field indicating the row (resource to track)",
+                required: true,
+                attributes: {
+                  options: fields.map((f) => f.name),
+                },
+              },
+              {
+                name: "row_label_formula",
+                label: "Row label formula",
+                class: "validate-expression",
+                type: "String",
+                sublabel: "Leave blank to use row field",
+              },
+              {
+                name: "start_field",
+                label: "Start time field",
+                type: "String",
+                sublabel:
+                  "The table needs a fields of type 'Date' to track start times.",
+                required: true,
+                attributes: {
+                  options: fields
+                    .filter((f) => f.type.name === "Date")
+                    .map((f) => f.name),
+                },
+              },
+              {
+                name: "end_field",
+                label: "End time field",
+                type: "String",
+                sublabel:
+                  "Specify an end field of type 'Date' or a duration field",
+                required: false,
+                attributes: {
+                  options: fields
+                    .filter((f) => f.type.name === "Date")
+                    .map((f) => f.name),
+                },
+              },
+
+              {
+                name: "duration_field",
+                label: "Duration field",
+                type: "String",
+                sublabel:
+                  "A fields of type 'Int' or 'Float' to denote the duration of the event.",
+                required: false,
+                attributes: {
+                  options: fields
+                    .filter(
+                      (f) =>
+                        f.type.name === "Integer" || f.type.name === "Float"
+                    )
+                    .map((f) => f.name),
+                },
+              },
+              {
+                name: "duration_units",
+                label: "Duration units",
+                type: "String",
+                sublabel: "Units of duration field, if specified",
+                required: true,
+                attributes: {
+                  options: "Seconds,Minutes,Hours,Days",
+                },
+              },
+            ],
+          });
+        }
+      }
+    ]
+  })
+
+const get_state_fields = async (table_id, viewname, { show_view }) => {
+  const table_fields = await Field.find({ table_id });
+  return table_fields
+    .filter((f) => !f.primary_key)
+    .map((f) => {
+      const sf = new Field(f);
+      sf.required = false;
+      return sf;
+    });
+};
+const run = async (
+  table_id,
+  viewname,
+  {
+
+    row_field,
+
+  },
+  state,
+  extraArgs
+) => {
+  const table = await Table.findOne({ id: table_id });
+  const fields = await table.getFields();
+  const row_fld = fields.find((f) => f.name === row_field);
+  readState(state, fields);
+
+  const role = extraArgs.req.isAuthenticated()
+    ? extraArgs.req.user.role_id
+    : 10;
+  const qstate = await stateFieldsToWhere({ fields, state });
+  if (
+    state[`_fromdate_${start_field}`] &&
+    state[`_todate_${start_field}`] &&
+    features?.prefix_or_in_queries
+  ) {
+    const from = new Date(state[`_fromdate_${start_field}`]);
+    const to = new Date(state[`_todate_${start_field}`]);
+    if (end_field)
+      qstate.or = [
+        { [start_field]: [{ gt: from }, { lt: to }] },
+        { [end_field]: [{ gt: from }, { lt: to }] },
+        {
+          [start_field]: { lt: from },
+          [end_field]: { gt: to },
+        },
+      ];
+    else if (duration_field) qstate[start_field] = [{ gt: from }, { lt: to }];
+    delete qstate[start_field];
+  }
+  let get_row_from_task = r => r[row_field]
+  const joinFields = {};
+  if (row_fld.is_fkey) {
+    joinFields[`summary_field_${row_fld.name}`] = {
+      ref: row_fld.name,
+      target: row_fld.attributes.summary_field,
+    };
+    get_row_from_task = r => r[`summary_field_${row_fld.name}`]
+  }
+  if (row_label_formula && add_free_variables_to_joinfields) {
+    add_free_variables_to_joinfields(
+      freeVariables(row_label_formula),
+      joinFields,
+      fields
+    );
+    get_row_from_task = r => eval_expression(row_label_formula, r)
+  }
+
+  const tasks = await table.getJoinedRows({
+    where: qstate,
+    joinFields,
+    //orderBy: (row_order_field || "").includes(".") ? "_order" : row_order_field,
+    //orderDesc: row_order_descending,
+  });
+  let first_start, last_end;
+  tasks.forEach(r => {
+    const to =
+      duration_field && r[duration_field]
+        ? moment(r[start_field]).add(
+          r[duration_field],
+          duration_units.toLowerCase()
+        )
+        : end_field && r[end_field]
+          ? r[end_field]
+          : moment(r[start_field]).add(1, "hour");
+    if (!first_start || r[start_field] < first_start)
+      first_start = r[start_field];
+    if (!last_end || to > last_end) last_end = to;
+    r._to = to
+  })
+
+  if (state[`_fromdate_${start_field}`])
+    first_start = new Date(state[`_fromdate_${start_field}`]);
+  if (state[`_todate_${start_field}`])
+    last_end = new Date(state[`_todate_${start_field}`]);
+  var spanDays = moment(last_end).diff(first_start, "days");
+  var spanMonths = moment(last_end).diff(first_start, "months");
+  //console.log({ spanDays, spanMonths });
+  const spanProps = calcSpanProps(spanMonths, spanDays)
+
+  const divid = `ganttres${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const resources = [...new Set(tasks.map(r => ({
+    id: r[row_field],
+    label: get_row_from_task(r)
+  })))]
+  let resTasks = []
+  resources.forEach(({ id, label }) => {
+
+  })
+
+  return ""
+}
+module.exports = {
+  configuration_workflow,
+  get_state_fields,
+  run,
+  name: "Gantt chart resources",
+}
+
+/* TODO
+
+2. figure out timescale, resource tracker division
+3. what are different rows? loop over them
+   a. what are my tasks
+   b. create arrays for time division counts
+   b. loop/create over tasks, increment arrays
+   c. turn arrays into tasks where non-zero
+      
+
+*/
